7052d23e5cc285808dec5e20370c7d1def03d01d
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-09-15 21:14:04 +0500

    t

--- school2/src/Navbar.js
+++ school2/src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from './navbar.module.css'
+import {MenuOutlined} from '@ant-design/icons'
 export default class Navbar extends Component {
     state={
         visible:false
@@ -25,14 +26,25 @@ export default class Navbar extends Component {
                         258-maktab
                     </div>
                 </div>
+                <div style={{position:'absolute',top:'15%',width:'100%'}}>
                 <div className={styles.mobileNavbar}>
                 <div className={styles.rightNavbar}>
                         258-maktab
                     </div>
                     <div className={styles.openHamburger}>
-                        {/* <GiHamburgerMenu onClick={()=>openNavbar()} styles={{fontSize:'20px',cursor:'pointer',color:'white'}}/> */}
+                    <MenuOutlined onClick={()=>this.openNavbar()} style={{color:'white',marginRight:'10px',fontSize:'25px',cursor:'pointer'}}/>
                     </div>
-                   
+                </div>
+                <div  className={this.state.visible? styles.navMobile:styles.noNav}>
+                    <ul style={{listStyleType:'none'}}>
+                            <li>Bosh sahifa</li>
+                            <li>Maktab hayoti</li>
+                            <li>Qabul</li>
+                            <li>Yangiliklar</li>
+                            <li>Maktab ma'muriyati</li>
+                            <li>Maktab a'lochilari</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
