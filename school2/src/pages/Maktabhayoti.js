678e3da68c782936e5ea4c61fa21dcf08620e568
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-09-16 23:31:37 +0500

    t

--- school2/src/pages/Maktabhayoti.js
+++ school2/src/pages/Maktabhayoti.js
@@ -9,11 +9,47 @@ import Slider from "react-slick";
 export default class Maktabhayoti extends Component {
     render() {
         const settings = {
+            autoplay:true,
             dots: true,
             infinite: true,
             speed: 500,
             slidesToShow: 3,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 1,
+                      infinite: true,
+                      dots: true
+                    }
+                  },
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+            ]
           }
         return (
             <div style={{overflow:'hidden'}}>
@@ -93,33 +129,61 @@ export default class Maktabhayoti extends Component {
                 </div>
                 <div className={styles.successful}>
                     <h1>Bizning muvaffaqiyatli o'quvchilarimiz</h1>
-                    <Slider {...settings}>
+                    <Slider {...settings} style={{padding:'20px'}}>
                      <div className={styles.slider}>
-                         <div style={{display:'flex',flexDirection:'row'}}>
-                             <div style={{width:'50px',height:'50px',borderRadius:'50%'}}>
-                                 <img src={school1} style={{width:'100%'}}/>
+                         <div style={{display:'flex',flexDirection:'row',padding:'30px'}}>
+                             <div style={{width:'80px'}}>
+                                 <img src={school1} style={{width:'80px',height:'80px',objectFit:'cover',borderRadius:'50%'}}/>
                              </div>
+                             <div style={{marginLeft:'10px'}}>
+                                 <h4 style={{marginTop:'10px'}}>Azizov Abror</h4>
+                                 <p style={{marginTop:'-5px',color:'#1EB2A6'}}>9-"B" sinf o'quvchisi</p>
+                             </div>
+                         </div>
+                         <div>
+                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
                          </div>
                      </div>
                      <div className={styles.slider}>
-                         <div style={{display:'flex',flexDirection:'row'}}>
-                             <div style={{width:'50px',height:'50px',borderRadius:'50%'}}>
-                                 <img src={school1} style={{width:'100%'}}/>
+                         <div style={{display:'flex',flexDirection:'row',padding:'30px'}}>
+                             <div style={{width:'80px'}}>
+                                 <img src={school1} style={{width:'80px',height:'80px',objectFit:'cover',borderRadius:'50%'}}/>
                              </div>
+                             <div style={{marginLeft:'10px'}}>
+                                 <h4 style={{marginTop:'10px'}}>Azizov Abror</h4>
+                                 <p style={{marginTop:'-5px',color:'#1EB2A6'}}>9-"B" sinf o'quvchisi</p>
+                             </div>
+                         </div>
+                         <div>
+                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
                          </div>
                      </div>
                      <div className={styles.slider}>
-                         <div style={{display:'flex',flexDirection:'row'}}>
-                             <div style={{width:'50px',height:'50px',borderRadius:'50%'}}>
-                                 <img src={school1} style={{width:'100%'}}/>
+                         <div style={{display:'flex',flexDirection:'row',padding:'30px'}}>
+                             <div style={{width:'80px'}}>
+                                 <img src={school1} style={{width:'80px',height:'80px',objectFit:'cover',borderRadius:'50%'}}/>
                              </div>
+                             <div style={{marginLeft:'10px'}}>
+                                 <h4 style={{marginTop:'10px'}}>Azizov Abror</h4>
+                                 <p style={{marginTop:'-5px',color:'#1EB2A6'}}>9-"B" sinf o'quvchisi</p>
+                             </div>
+                         </div>
+                         <div>
+                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
                          </div>
                      </div>
                      <div className={styles.slider}>
-                         <div style={{display:'flex',flexDirection:'row'}}>
-                             <div style={{width:'50px',height:'50px',borderRadius:'50%'}}>
-                                 <img src={school1} style={{width:'100%'}}/>
+                         <div style={{display:'flex',flexDirection:'row',padding:'30px'}}>
+                             <div style={{width:'80px'}}>
+                                 <img src={school1} style={{width:'80px',height:'80px',objectFit:'cover',borderRadius:'50%'}}/>
                              </div>
+                             <div style={{marginLeft:'10px'}}>
+                                 <h4 style={{marginTop:'10px'}}>Azizov Abror</h4>
+                                 <p style={{marginTop:'-5px',color:'#1EB2A6'}}>9-"B" sinf o'quvchisi</p>
+                             </div>
+                         </div>
+                         <div>
+                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
                          </div>
                      </div>
                     </Slider>
