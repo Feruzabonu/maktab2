4b1eacb109b7cdc9e4dd9a46b823e5ded15aad45
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-09-16 21:10:37 +0500

    t

--- school2/src/App.js
+++ school2/src/App.js
@@ -1,11 +1,25 @@
 import Navbar from './Navbar'
 import './App.css';
 import Boshsahifa from './Boshsahifa';
-
+import Maktabhayoti from './Maktabhayoti';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Boshsahifa/>
+      <BrowserRouter>
+      <Switch>
+      <Route exact path="/">
+         <Boshsahifa />
+      </Route>
+      <Route exact path="/home">
+         <Boshsahifa />
+      </Route>
+      <Route exact path="/maktabhayoti">
+        <Navbar/>
+         <Maktabhayoti/>
+      </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ school2/src/Maktabhayoti.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import styles from './about.module.css'
+export default class Maktabhayoti extends Component {
+    render() {
+        return (
+            <div>
+                <div className={styles.header}>
+                 
+                </div>
+            </div>
+        )
+    }
+}
